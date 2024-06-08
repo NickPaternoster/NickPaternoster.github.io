9725c2a43d6451a45ec008a26e17937b2d6dafb8
Nick Paternoster <nickolaspaternoster@gmail.com>
2024-06-08 19:25:21 -0400

    Mostly minor cv revisions

--- src/views/Awards.js
+++ src/views/Awards.js
@@ -1,63 +1,121 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import React from 'react';
 import '../styles/PubsStyle.css';
 import cornellSeal from '../images/cornell_seal.png';
 import opamSeal from '../images/opam.png';
 import nsfSeal from '../images/nsf.png';
 
 export default function Awards() {
+  const talks = [
+    {
+      year: "2021",
+      date: "Nov. 2021",
+      details: "Eye movements to targets and distractors: insights into the size congruity effect in visual search.",
+      event: "Object Perception, Visual Attention, and Visual Memory",
+      speakers: ["Paternoster, N.", "Andrews, A.", "Williams, J.", "Dague, T.", "Sobel, K. V.", "Puri, A."],
+      role: "[Speaker]"
+    },
+    {
+      year: "2021",
+      date: "Nov. 2021",
+      details: "Effects of set size on ensemble processing of semantic information.",
+      event: "Chalk Talk, College of Natural Sciences and Mathematics, University of Central Arkansas",
+      speakers: ["Paternoster, N.", "Andrews, E.", "Lee, K.", "Sobel, K. V.", "Puri, A."],
+      role: "[Speaker]"
+    },
+    {
+      year: "2020",
+      date: "Nov. 2020",
+      details: "The processing pathway of interaction between semantic and perceptual information.",
+      event: "Research and Industry Day. Louisiana State University",
+      speakers: ["Paternoster, N.", "Lee, K.", "Dague, T.", "Sobel, K. V.", "Puri, A."],
+      role: "[Speaker]",
+      award: "(Third Place Award)"
+    }
+  ];
+
+  const posters = [
+    {
+      year: "2022",
+      date: "Nov. 2022",
+      details: "Eye movements reveal interactions between the SNARC and size congruity effects.",
+      event: "Object Perception, Visual Attention, and Visual Memory",
+      speakers: ["Paternoster, N.", "Wilson, N.T.", "Andrews, E.R.", "Sobel, K. V.", "Puri, A."],
+      role: "[Presenter]"
+    },
+    {
+      year: "2022",
+      date: "May 2022",
+      details: "Interaction Between SNARC and Size Congruity Effects Revealed by Eye Movements.",
+      event: "Vision Sciences Society Annual Meeting",
+      speakers: ["Puri, A.", "Dague, T.", "Sobel, K.", "Paternoster, N."],
+      role: "[Co-Author]"
+    },
+    {
+      year: "2021",
+      date: "Nov. 2021",
+      details: "The Spatial Resolution of Object Discrimination Across Echolocation and Touch Interpreting sensation: Quantifying the bottleneck of information transfer between echolocation.",
+      event: "Object Perception, Visual Attention, and Visual Memory",
+      speakers: ["Dacus, C.", "Paternoster, N.", "Ezeana, M.", "Teng, S.", "& Puri, A."],
+      role: "[Co-Author]",
+      award: "(First Place Award)"
+    },
+    {
+      year: "2021",
+      date: "Oct. 2021",
+      details: "Slower Eye Movements to Incongruent Targets May Underlie the SCE In Digit Perception.",
+      event: "Arkansas Idea Network of Biomedical Research Excellence",
+      speakers: ["Paternoster, N.", "Ezeana, M.", "Teng, S.", "& Puri, A."],
+      role: "[Presenter]"
+    },
+    {
+      year: "2021",
+      date: "May 2021",
+      details: "Eye movements and interactions between numerical and physical size in visual search for digits.",
+      event: "Vision Sciences Society Annual Meeting",
+      speakers: ["Puri, A.", "Dague, T.", "Sobel, K.", "Paternoster, N."],
+      role: "[Co-Author]"
+    },
+    {
+      year: "2021",
+      date: "May 2021",
+      details: "Is There a SNARC Effect in The Size Congruity Effect?.",
+      event: "Vision Sciences Society Annual Meeting",
+      speakers: ["Wilson, N.T.", "Paternoster, N.", "Dacus, C.", "Sobel, K.", "& Puri, A."],
+      role: "[Co-Author]"
+    },
+    {
+      year: "2020",
+      date: "Oct. 2020",
+      details: "Effects of set size on ensemble processing of semantic information.",
+      event: "Research and Industry Day. Louisiana State University",
+      speakers: ["Paternoster, N.", "Lee, K.", "Dague, T.", "Sobel, K. V.", "Puri, A."],
+      role: "[Presenter]",
+      award: "(Third Place Award)"
+    }
+  ];
 
   return (
     <div className="parent-container">
-      <div className="pubs-title">Awards</div>
-
-      
+      <div className="presenter-title">Awards</div>
       <div className="award-entry">
         <div className="award-images">
-        <img src={cornellSeal} alt="Cornell University Seal" className="award-image" />
+          <img src={cornellSeal} alt="Cornell University Seal" className="award-image" />
           <img src={nsfSeal} alt="NSF Seal" className="award-image" />
           <img src={opamSeal} alt="OPAM Seal" className="award-image" />
         </div>
       </div>
-        
-
-
-      <div className="pubs-title">Conference Presentations</div>
-
-      <div>
-        Paternoster, N., Andrews, A., Williams, J., Dague, T., Sobel, K. V., & Puri, A. (Nov. 2021). Eye movements to targets and distractors: insights into the size congruity effect in visual search. [Speaker] Object Perception, visual Attention, and visual Memory.
-      </div>
-
-      <div>
-        Paternoster, N., Andrews, E., Lee, K., Sobel, K. V., & Puri, A. (Nov. 2021). Effects of set size on ensemble processing of semantic information. [Speaker] Chalk Talk, College of Natural Sciences and Mathematics, University of Central Arkansas.
-      </div>
-
-    <div>
-        Paternoster, N., Lee, K., Dague, T., Sobel, K. V., & Puri, A. (Nov. 2020). Visual search of digits and the processing pathway of interaction between semantic and perceptual information. [Speaker] Research and Industry Day. Louisiana State University. (Third Place Award)
-      </div>
-      <div className="pubs-title">Conference Posters</div>
-      <div>
-        Paternoster, N., Wilson, N.T., Andrews, E.R., Sobel, K. V., & Puri, A. (Nov. 2022). Eye movements reveal interactions between the SNARC and size congruity effects. [Presenter] Object Perception, visual Attention, and visual Memory.
-      </div>
-
-      <div>
-        Puri, A., Dague, T., Sobel, K., Paternoster, N. (May 2022). Interaction Between SNARC and Size Congruity Effects Revealed by Eye Movements. [Co-Author] Vision Sciences Society Annual Meeting. 
-      </div>
-      <div>
-        Dacus, C., Paternoster, N., Ezeana, M., Teng, S., & Puri., A. (Nov. 2021). The Spatial Resolution of Object Discrimination Across Echolocation and Touch Interpreting sensation: Quantifying the bottleneck of information transfer between echolocation. [Co-Author] Object Perception, visual Attention, and visual Memory. (First Place Award) 
-      </div>
-      <div>
-        Paternoster, N., Ezeana, M., Teng, S., & Puri., A. (Oct. 2021). Slower Eye Movements to Incongruent Targets May Underlie the SCE In Digit Perception. [Presenter] Arkansas Idea Network of Biomedical Research Excellence
-      </div>
-      <div>
-        Puri, A., Dague, T., Sobel, K., Paternoster, N. (May 2021). Eye movements and interactions between numerical and physical size in visual search for digits. [Co-Author] Vision Sciences Society Annual Meeting.  
-      </div>
-      <div>
-        Wilson, N.T., Paternoster, N., Dacus, C., Sobel, K., & Puri., A. (May 2021). Is There a SNARC Effect in The Size Congruity Effect?. [Co-Author] Vision Sciences Society Annual Meeting.  
-      </div>
-      <div>
-        RAID - Paternoster, N., Lee, K., Dague, T., Sobel, K. V., & Puri, A. (Oct. 2020). Effects of set size on ensemble processing of semantic information. [Presenter] Research and Industry Day. Louisiana State University. 
-      </div>
+      <div className="presenter-title">Conference Presentations</div>
+      {talks.map((talk, index) => (
+        <div key={index}>
+         <p className="centered-text-conferences"> <strong>{talk.details}</strong><br></br>{talk.role}<br></br> {talk.event}. {talk.date}. {talk.award || ''}</p>
+        </div>
+      ))}
+      <div className="presenter-title">Conference Posters</div>
+      {posters.map((poster, index) => (
+        <div key={index}>
+          <p className="centered-text-conferences"><strong>{poster.details}</strong><br></br>{poster.role}<br></br>{poster.event}. {poster.award || ''}</p>
+        </div>
+      ))}
     </div>
   );
 }
--- src/views/Pubs.js
+++ src/views/Pubs.js
@@ -1,30 +1,78 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { NavLink } from 'react-router-dom';
 import '../styles/PubsStyle.css';
+import downloadIcon from '../images/highlight_download.png';
+import highlightDownloadIcon from '../images/download_highlight.png';
+
 
 export default function Pubs() {
+  const publications = [
+    {
+      year: "2024",
+      description: "Object recognition via echoes: Quantifying the crossmodal transfer of three-dimensional shape information between echolocation, vision, and haptics.",
+      authors: ["Teng, S.", "Danforth, C.", "Paternoster, N. J.", "Ezeana, M.", "& Puri, A. M."],
+      link: "https://www.frontiersin.org/journals/neuroscience/articles/10.3389/fnins.2024.1288635/full",
+      journal: "Frontiers in Neuroscience"
+    },
+    {
+      year: "2021",
+      description: "Set size and ensemble perception of numerical value.",
+      authors: ["Lee, K. R.", "Dague, T. D.", "Sobel, K. V.", "Paternoster, N. J.", "& Puri, A. M."],
+      link: "https://link.springer.com/article/10.3758/s13414-020-02196-8",
+      journal: "Attention, Perception, & Psychophysics"
+    }
+  ];
+  const [hoverDownloads, setHoverDownloads] = useState(new Array(publications.length).fill(false));
+
+  const renderAuthors = (authors) => authors.map((author, index) => 
+    author.includes("Paternoster, N. J.") ? <strong key={index}>{author}</strong> : <span key={index}>{author}</span>
+  );
+
   return (
     <div className="parent-container">
       <div className="pubs-title">Journal Publications</div>
-      <div className="year-container">
-        <div className="year">2024</div>
-        <div className="publication-details">
-          <p>Teng, S., Danforth, C., Paternoster, N., Ezeana, M., & Puri, A. (2024). Object recognition via echoes: Quantifying the crossmodal transfer of three-dimensional shape information between echolocation, vision, and haptics. <em>Frontiers in Neuroscience, 18.</em> https://doi.org/10.3389/fnins.2024.1288635</p>
+      {publications.map((pub, index) => (
+        <div className="year-container" key={index}>
+          <div className="year">{pub.year}</div>
+          <div className="publication-details">
+            <p>{pub.description}</p>
+            <p>{renderAuthors(pub.authors).reduce((prev, curr) => [prev, ", ", curr])}</p>
+            <div className="button-link-container">
+              <button
+                className="download-button"
+                onMouseEnter={() => {
+                  const updated = [...hoverDownloads];
+                  updated[index] = true;
+                  setHoverDownloads(updated);
+                }}
+                onMouseLeave={() => {
+                  const updated = [...hoverDownloads];
+                  updated[index] = false;
+                  setHoverDownloads(updated);
+                }}
+              >
+                <img src={hoverDownloads[index] ? highlightDownloadIcon : downloadIcon} alt="Download" />
+              </button>
+              <span className="separator">|</span>
+              <a href={pub.link} className="frontiers-button">Read on {pub.journal}</a>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="year-container">
-        <div className="year">2021</div>
-        <div className="publication-details">
-          <p>Lee, K. R., Dague, T. D., Sobel, K. V., Paternoster, N. J., & Puri, A. M. (2021). Set size and ensemble perception of numerical value. <em>Attention, Perception, & Psychophysics, 83(3)</em>, 1169–1178. <a href="https://doi.org/10.3758/s13414-020-02196-8">https://doi.org/10.3758/s13414-020-02196-8</a></p>
-        </div>
-      </div>
+      ))}
       <div className="pubs-title">In-Preparation</div>
       <div className="year-container">
-        <div className="publication-details">
-          <p>Paternoster, N. J., Dague, T. D., Sobel, K. V., Puri, A. M., Eye movements reveal an influence of semantic-perceptual congruity on attentional selection in visual search</p>
+
+      <div className="publication-details">
+          <p>Investigating the effects of the noradrenergic system on visual processing</p>
+          <p><strong>Paternoster, N. J.</strong>, Swallow, K. M.</p>
         </div>
-      </div>
+
+        <div className="publication-details">
+          <p>Eye movements reveal an influence of semantic-perceptual congruity on attentional selection in visual search</p>
+          <p><strong>Paternoster, N. J.</strong>, Dague, T. D., & Puri, A. M.</p>
+        </div>
       <NavLink exact to="/research" className="button-below">Get More Information</NavLink>
+    </div>
     </div>
   );
 }
